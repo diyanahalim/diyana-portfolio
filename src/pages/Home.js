3f7f2eb1a7679e346a03a498171e36bfc72661b0
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-07-24 18:32:55 +0800

    Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
           Diyana's Portfolio
         </h1>
         <h2 className="introduction-description">
-          Frontend Developer
+          Frontend Developer 
         </h2>
         <div className="skills-container">
           <img src={skill1} className="skill-logo" alt="Logo" />
