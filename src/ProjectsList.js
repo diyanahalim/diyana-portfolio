3a865784af157eef2359443fe7a01aaf33db43e4
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-07-27 13:29:07 +0800

    Changes

--- src/ProjectsList.js
+++ src/ProjectsList.js
@@ -11,28 +11,22 @@ import img9 from './images/Todo App/3.jpg'
 export const projects = [{
     id: 0,
     project_index: '01',
-    // project_image_1: img1,
-    // project_image_2: img2,
-    // project_image_3: img3,
     project_image: [img1, img2, img3],
     project_title: '01. Online Booking Website',
-    project_description: 'A responsive website that was built with ReactJS+node.js. This visually stunning website showcases a breakdown of each treatment along with its price and "Book Now" buttons to direct users to a booking form. The booking data will then be received using Express in Node.js. Some of other features that are included in this online spa booking website are customer reviews, image gallery, contact information and FAQ.'
+    project_description: 'A responsive website that was built with ReactJS+node.js. This visually stunning website showcases a breakdown of each treatment along with its price and "Book Now" buttons to direct users to a booking form. The booking data will then be received using Express in Node.js. Some of other features that are included in this online spa booking website are customer reviews, image gallery, contact information and FAQ.',
+    link: 'https://beauty-spa-co-3-client.vercel.app',
 }, {
     id: 1,
     project_index: '02',
-    // project_image_1: img4,
-    // project_image_2: img5,
-    // project_image_3: img6,
     project_image: [img4, img5, img6],
     project_title: '02. Finding Tutor Website',
-    project_description: 'This simple and minimalistic website was built with ReactJS for tutoring business. It acts as a platform to share the list of available tutors with their education and credentials. It also lets the prospective students and families to know what services and subjects the business supports.'
+    project_description: 'This simple and minimalistic website was built with ReactJS for tutoring business. It acts as a platform to share the list of available tutors with their education and credentials. It also lets the prospective students and families to know what services and subjects the business supports.',
+    link: 'https://tutoreal.vercel.app',
 }, {
     id: 3,
     project_index: '03',
-    // project_image_1: img7,
-    // project_image_2: img8,
-    // project_image_3: img9,
     project_image: [img7, img8, img9],
     project_title: '03. Todo List App',
-    project_description: 'Just another typical todo-list app but with a nice touch of futuristic design. This app was built with ReactJS and allows users to add tasks, mark tasks as completed, edit or delete task and view the task.'
+    project_description: 'Just another typical todo-list app but with a nice touch of futuristic design. This app was built with ReactJS and allows users to add tasks, mark tasks as completed, edit or delete task and view the task.',
+    link: 'https://tutoreal.vercel.app',
 }];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,10 @@
 import React, { useEffect, useState } from 'react'
-// import { gsap } from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-// import SplitType from 'split-type';
 import { projects } from '../ProjectsList.js';
 import '../pages/Projects.css'
-// import first_project_1 from '../images/Online Booking Website/1.jpg'
-// import first_project_2 from '../images/Online Booking Website/2.jpg'
-// import first_project_3 from '../images/Online Booking Website/3.jpg'
-// import profile_image from '../images/diyana.png'
 
 const Projects = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  // const [imageIndex, setImageIndex] = useState(0);
 
- 
-  // window.addEventListener('DOMContentLoaded', callback());
   useEffect(() => {
     
   var myIndex = 0;
@@ -43,18 +33,8 @@ const Projects = () => {
     }
     window.addEventListener("resize", () => {
       const ismobile = window.innerWidth < 768;
-      // const carousel = () => {
-      //   setImageIndex((prev) => (prev < 2 ? prev + 1 : 0));
-      // };
       if (ismobile !== isMobile) {
         setIsMobile(ismobile);  
-        // carousel();
-        // const timer = setInterval(() => {
-        //   carousel();
-        // }, 2000); // set the which timer you want
-        // return () => {
-        //   clearInterval(timer);
-        // };
       }
         
   }, false);
@@ -68,51 +48,22 @@ const Projects = () => {
         if(entry.isIntersecting)
         {
             entry.target.classList.add('show');
-            // observer.unobserve(entry.target)
           }
     });
 }
-
-
-
-  // window.addEventListener('DOMContentLoaded', setup);
-  // function setup() {
-  //   const options = {
-  //     rootMargin: '0px 0px -200px 0px'
-  //   }
-  //   const observer = new IntersectionObserver((entries, observer) => {
-  //     entries.forEach(entry => {
-  //       if(entry.isIntersecting){
-  //         entry.target.classList.add('show');
-  //         observer.unobserve(entry.target);
-  //       } else {
-  //         return;
-  //       }
-  //     })
-  //   }, options);
-
-    // const h1 = document.querySelector('h1');
-    // observer.observe(h1);
-  //   const paras = document.querySelectorAll('.grid-right-side h4');
-  //   paras.forEach(h4 => observer.observe(h4));
-    
-  //   const paras2 = document.querySelectorAll('.grid-right-side p');
-  //   paras2.forEach(p => observer.observe(p));
-  // }
   
   const projectItems = projects.map(project =>
     <div className="grid-container" key={project.id}>
       <div id="grid" className="grid">
           <h1 className="project-index">{project.project_index}</h1>
-            
-          <div className="img-container">
-            <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[0]} alt="Logo" />
-            <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[1]} alt="Logo" />
-            <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[2]} alt="Logo" />
-            {/* <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image_1} alt="Logo" />
-            <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image_2} alt="Logo" />
-            <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image_3} alt="Logo" /> */}
-          </div>
+
+          <a href={project.link} target="_blank">
+            <div className="img-container">
+              <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[0]} alt="Logo" />
+              <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[1]} alt="Logo" />
+              <img className={`${isMobile ? "image-mobile" : "image"}`} src={project.project_image[2]} alt="Logo" />
+            </div>
+          </a>  
       </div>
       <div id="target" className="grid-description show-onscroll">
       <h4 className="project-title">{project.project_title}</h4>
@@ -122,46 +73,6 @@ const Projects = () => {
 
     
   );
-
-  // useEffect(() => {
-  //   gsap.config({ trialWarn: false });
-  //   gsap.registerPlugin(ScrollTrigger);
-
-  //   const split = new SplitType('.grid-container .grid-right-side', { type: 'chars' }); // Use SplitType
-
-  //   const anim = gsap.to(split.chars, {
-  //     //   delay: 1,
-  //     color: 'rgb(1, 0, 254)', // Change color to white
-  //     stagger: 1,
-  //     ease: 'none',
-  //     scrollTrigger: {
-  //       trigger: '.grid-container',
-  //       start: 'top center',
-  //       end: 'bottom center',
-  //       scrub: true,
-  //       markers: true,
-  //     },
-  //   });
-    
-  // ScrollTrigger.refresh()
-  // }, []);
-
-//   gsap.registerPlugin(ScrollTrigger);
-//   loop through each "h2" element and create an animation/ScrollTrigger for each
-// gsap.utils.toArray(".grid-right-side").forEach(element => {
-//   gsap.to(element, {
-//     opacity: 1, 
-//     ease: "none",
-//     scrollTrigger: {
-//       trigger: element,
-//       start: "top center",
-//       end: "bottom center",
-//       markers: true,
-//       toggleActions: "play reverse play reverse"
-//     }
-//   })
-//   ScrollTrigger.refresh()
-// });
 
   return (
     
