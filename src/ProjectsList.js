9e56938f89ada72a6b3539bff4ae4828f7eb46a5
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-09-19 11:29:23 +0800

    changes

--- src/ProjectsList.js
+++ src/ProjectsList.js
@@ -7,26 +7,37 @@ import img6 from './images/Tuto Website/3.jpg'
 import img7 from './images/Todo App/1.jpg'
 import img8 from './images/Todo App/2.jpg'
 import img9 from './images/Todo App/3.jpg'
+import img10 from './images/Herstory Dashboard/1.jpg'
+import img11 from './images/Herstory Dashboard/2.jpg'
+import img12 from './images/Herstory Dashboard/3.jpg'
 
 export const projects = [{
     id: 0,
     project_index: '01',
+    project_image: [img10, img11, img12],
+    project_title: '01. Data Visualisation on Sexual Harassment Cases',
+    project_description: 'Built with Python with Dash, Herstory summarises the large sets of information regrading sexual harassment and crimes in New York City through interactive visualisation. In this dashboard, various charts and statistics are included to analyse trends and patterns of the crime.',
+    link: 'https://abacus-laptop-shear-d3yd.onrender.com',
+},
+    {
+    id: 1,
+    project_index: '02',
     project_image: [img1, img2, img3],
-    project_title: '01. Online Booking Website',
+    project_title: '02. Online Booking Website',
     project_description: 'A responsive website that was built with ReactJS+node.js. This visually stunning website showcases a breakdown of each treatment along with its price and "Book Now" buttons to direct users to a booking form. The booking data will then be received using Express in Node.js. Some of other features that are included in this online spa booking website are customer reviews, image gallery, contact information and FAQ.',
     link: 'https://beauty-spa-co-3-client.vercel.app',
 }, {
-    id: 1,
-    project_index: '02',
+    id: 2,
+    project_index: '03',
     project_image: [img4, img5, img6],
-    project_title: '02. Finding Tutor Website',
+    project_title: '03. Finding Tutor Website',
     project_description: 'This simple and minimalistic website was built with ReactJS for tutoring business. It acts as a platform to share the list of available tutors with their education and credentials. It also lets the prospective students and families to know what services and subjects the business supports.',
     link: 'https://tutoreal.vercel.app',
 }, {
     id: 3,
-    project_index: '03',
+    project_index: '04',
     project_image: [img7, img8, img9],
-    project_title: '03. Todo List App',
+    project_title: '04. Todo List App',
     project_description: 'Just another typical todo-list app but with a nice touch of futuristic design. This app was built with ReactJS and allows users to add tasks, mark tasks as completed, edit or delete task and view the task.',
-    link: 'https://react-to-do-app-one-beryl.vercel.app/',
+    link: 'https://react-to-do-app-one-beryl.vercel.app',
 }];
